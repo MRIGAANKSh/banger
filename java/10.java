6b3c606042547d206a0c26390fa190c9b815b26d
MRIGAANKSh <mrigaanksharma928@gmail.com>
2025-02-18 14:48:39 +0530

    java done

---
+++ java/10.java
@@ -0,0 +1,49 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("eneter numbers 1");
+        int a=sc.nextInt();
+        System.out.println("eneter numbers 2");
+        int b=sc.nextInt();
+        if(a>b){
+            System.out.println("ais grester");
+        }
+        else{
+            System.out.println("b is greater");
+        }
+        
+    }
+}
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("eneter numbers 1");
+        int a=sc.nextInt();
+        switch(a){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Mony");
+                break;
+            case 3:
+                System.out.println("Mnday");
+                break;
+            case 4:
+                System.out.println("Moday");
+                break;
+            default:
+            System.out.println("nday");
+            break;
+        
+        }
+        
+    }
+}
---
+++ java/9.java
@@ -0,0 +1,36 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Input age");
+        int age=sc.nextInt();
+        System.out.println(age);
+    }
+}
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("eneter ur name");
+        String name=sc.next();
+        System.out.println(name);
+    }
+}
+
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("eneter ur name");
+        String name=sc.nextLine();
+        System.out.println(name);
+    }
+}
