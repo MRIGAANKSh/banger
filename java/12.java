2c688ff36342c842382d49d461238c600456529c
MRIGAANKSh <mrigaanksharma928@gmail.com>
2025-02-19 13:08:08 +0530

    donedn

---
+++ java/12.java
@@ -0,0 +1,44 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++){
+            if(i==2){
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
+
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        int[] mark={99,55,66};
+        try{
+            System.out.println(mark[5]);
+        }
+        catch(Exception exception){
+            
+        }
+        System.out.println("hi");
+    }
+}
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+       printjava("hi");
+       printjava("mrigaank");
+        
+    }
+    public static void printjava(String name){
+        System.out.println(name);
+    }
+}
