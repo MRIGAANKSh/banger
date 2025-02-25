bd96c90060681a7562c752d24306ff6e06d4006d
MRIGAANKSh <mrigaanksharma928@gmail.com>
2025-02-25 16:11:38 +0530

    done

---
+++ Types/dist/script.js
@@ -0,0 +1,18 @@
+"use strict";
+//types
+//primtive datatypes
+//number boolean undefined string char symbols 
+//objects tuples
+// enum human {
+//     name='max'
+// }
+// console.log(human.name)
+// let a=12;
+// type City{
+//     section:"ghaziabad"
+// }
+// type Age{
+//     age:12;
+// }
+// type cs=City & Age;
+let a = 21;
---
+++ Types/script.ts
@@ -0,0 +1,23 @@
+//types
+//primtive datatypes
+//number boolean undefined string char symbols 
+//objects tuples
+
+// enum human {
+//     name='max'
+// }
+// console.log(human.name)
+
+// let a=12;
+// type City{
+//     section:"ghaziabad"
+// }
+
+// type Age{
+//     age:12;
+// }
+
+// type cs=City & Age;
+
+
+let a=21;
